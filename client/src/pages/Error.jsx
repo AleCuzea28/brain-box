0cfb067cbd090351725d20404648de65f5d8487d
alexandracuzea <gabriela.cuzea@student.upt.ro>
2023-12-08 11:30:00 +0200

    added wrappers

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,4 +1,13 @@
+import { Link, useRouteError } from "react-router-dom";
+
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div>
+      <h1>Error</h1>
+      <Link to="./dashboard"> back home</Link>
+    </div>
+  );
 };
 export default Error;
