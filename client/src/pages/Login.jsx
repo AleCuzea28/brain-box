5e0eeaf400003b4e72024dfb5717248af8be45d6
alexandracuzea <gabriela.cuzea@student.upt.ro>
2023-12-08 15:41:23 +0200

    created login and register pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,31 @@
+import { FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import main2 from "../assets/images/main2.png";
+
 import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <Link to="/register"> Register</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <img src={main2} alt="brain box logo" className="main2" />
+        <h4>Login</h4>
+        <FormRow type="email" name="email" defaultValue="ale@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="secret123" />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <button type="button" className="btn btn-block">
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow } from "../components";
 import main2 from "../assets/images/main2.png";
 
 const Register = () => {
@@ -8,26 +9,16 @@ const Register = () => {
       <form className="form">
         <img src={main2} alt="brain box logo" className="main2" />
         <h4>Register</h4>
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="form-input"
-            placeholder="name"
-            required
-          ></input>
-        </div>
-
+        <FormRow type="text" name="name" />
+        <FormRow type="text" name="lastName" labelText="Last Name" />
+        <FormRow type="email" name="email" />
+        <FormRow type="password" name="password" />
         <button type="submit" className="btn btn-block">
           submit
         </button>
         <p>
           Already a member?
-          <Link to="./login" className="member-btn">
+          <Link to="/login" className="member-btn">
             Login
           </Link>
         </p>
